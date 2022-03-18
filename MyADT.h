a0eedb084eecbdcace96e37b64b90af7421b9cb1
HX-Huax <90329050+HX-Huax@users.noreply.github.com>
2022-03-18 18:07:14 +0800

    Add files via upload

---
+++ MyADT.h
@@ -0,0 +1,9 @@
+//
+// Created by HuaXue on 2022/3/17.
+//
+
+#ifndef CORCPP_MYADT_H
+#define CORCPP_MYADT_H
+#include "List.h"
+
+#endif //CORCPP_MYADT_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "MyADT.h"
+#include<algorithm>
+int b[1000];
+int main()
+{
+    List<int> s;
+    s.ListRequire(100);
+
+
+    return 0;
+}
